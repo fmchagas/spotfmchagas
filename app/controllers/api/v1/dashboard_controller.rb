2f8fe1ae685977e750199ba964b6dc0506763b69
Fernando M. Chagas <fermaccha@gmail.com>
2020-04-17 22:50:21 -0400

    correct recommend album

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -7,17 +7,16 @@ class Api::V1::DashboardController < ApplicationController
   private
 
   def load_recent_heard
-    @recent_albums = current_user.recently_heards.order('created_at DESC').limit(4).map(&:album)
+    @recent_albums = current_user.recently_heards.order('created_at DESC').limit(4).map(&:album).uniq
   end
 
   def load_recommendations
     heard_categories = @recent_albums.map(&:category)
-    
     if heard_categories.present?
-      @recommend_albums = Album.joins(:category, :songs).where(category: heard_categories).order('songs.palyed_count')
-                                                        .select('distinct albums.*').limit(12)
+        @recommend_albums = Album.joins(:category, :songs).where(category: heard_categories).order('songs.played_count')
+                                                          .select('distinct albums.*').limit(12)
     else
-      @recommend_albums = Album.all.sample(12)
-    end
+        @recommend_albums = Album.all.sample(12)
+    end 
   end
 end
