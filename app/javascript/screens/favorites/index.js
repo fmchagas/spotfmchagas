67c00ed8b17f8694a9e6fed36f60a66756b328b5
Fernando M. Chagas <fermaccha@gmail.com>
2020-04-18 22:09:22 -0400

    Created the favorites page

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,13 +1,18 @@
 import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/common/section_wrapper';
 import NavbarFooter from '../../components/common/navbar_footer';
+import { Heading } from 'react-bulma-components';
+import Favorites from '../../components/favorites';
 
 const FavoritesScreen = () => {
-  return(
+  return (
     <Fragment>
-      <div>Favorite Screen</div>
-      <NavbarFooter />  
+      <SectionWrapper>
+        <Heading className='has-text-centered has-text-white'>Favoritos</Heading>
+        <Favorites />
+      </SectionWrapper>
+      <NavbarFooter />
     </Fragment>
-  )
+  );
 }
-
 export default FavoritesScreen;
