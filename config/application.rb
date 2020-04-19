37da3237e5ae81a6c5a239494892da30caf12ac6
Fernando M. Chagas <fermaccha@gmail.com>
2020-04-18 23:05:54 -0400

    Confige the PWA in App

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Spotfmchagas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.hosts << "7e076c24.ngrok.io"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
